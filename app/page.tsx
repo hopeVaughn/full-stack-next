aed50c5c0b53a6a78e81397c2905abd95279c1ff
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-22 13:04:25 -0800

    finish functionality for basic full stack app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Link from 'next/link'
+import FormPost from "./Form"
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const data: { id: number, title: string }[] = await getPosts()
+  console.log(data);
+
   return (
-    <main className='py-4 px-48'>
-      <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" href={'/dashboard'}>Go to the dashboard</Link>
+    <main className='py-8 px-48'>
+      <FormPost />
+      {/* <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" href={'/dashboard'}>Go to the dashboard</Link> */}
+      {data.map((post) => (
+        <h1 key={post.id} className='text-lg py-6'>{post.title}</h1>
+      ))}
     </main>
   )
 }
