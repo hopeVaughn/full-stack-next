2584c955c867bf85b28ac2b04001ea3bbe5d56f2
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-22 11:44:13 -0800

    add ts-node and prisma to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import styles from './page.module.css'
-
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main >
-      <h1>Home</h1>
+    <main className='py-4 px-48'>
+      <Link className="bg-teal-500 text-black font-medium py-2 px-4 rounded-md" href={'/dashboard'}>Go to the dashboard</Link>
     </main>
   )
 }
